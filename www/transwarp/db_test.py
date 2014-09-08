3165291f38129b30778faf568e1cc53670175780
hunterXiao <hunter.m.xiao@gmail.com>
2014-09-08 17:03:26 +0800

    edit orm
    complete orm.py

---
+++ www/transwarp/db_test.py
@@ -0,0 +1,235 @@
+#-*- coding:utf8 -*-
+
+import logging,threading,functools
+
+class Dict(dict):
+	"""docstring for Dict"""
+	def __init__(self, names=(),values=(),**kw):
+		super(Dict, self).__init__(**kw)
+		for k , v in zip(names,values):
+			self[k]=v
+
+	def __getattr__(self,key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError('Dict has no attribute %s'%key)
+		
+	def __setattr__(self,key,value):
+		self[key]=value
+
+
+class _LazyConnect(object):
+	"""docstring for _LazyConnect"""
+	def __init__(self):
+		self.connection = None
+	
+	def cursor(self):
+		if self.connection is None:
+			connection=engine.connect()
+			self.connection=connection
+		return self.connection.cursor()
+
+	def rollback(self):
+		self.connection.rollback()
+
+	def commit(self):
+		self.connection.commit()
+
+	def cleanup(self):
+		if self.connection:
+			connection=self.connection
+			self.connection=None
+			connection.close()
+
+class _DbCtx(threading.local):
+	def __init__(self):
+		self.connection=None
+		self.transaction=0
+
+	def is_init(self):
+		return not self.connection is None
+
+	def init(self):
+		self.connection=_LazyConnect()
+		self.transaction=0
+
+	def cursor(self):
+		return self.connection.cursor()
+
+	def cleanup(self):
+		self.connection.cleanup()
+
+_db_ctx = _DbCtx()
+
+engine=None
+
+class _Engine(object):
+	"""docstring for _Engine"""
+	def __init__(self, connect):
+		self._connect = connect
+	def connect(self):
+		return self._connect()
+
+def create_engine(user,password,host,database,port,**kw):
+	import mysql.connector
+	if engine is not None:
+		raise DBError('DB engine has already initizilied!')
+	params = dict(user=user,password=password,host=host,database=database,port=port)
+	defaults = dict(use_unicode=True,charset='utf8',collation='utf8_general_ci',autocommit=False)
+	for k,v in defaults.iteritems():
+		params[k]=kw.pop(k,v)
+	params.update(kw)
+	params['buffered']=True
+	engine=_Engine(lambda:mysql.connector.connect(**params))
+
+class _ConnectionCtx(object):
+	"""docstring for _ConnectionCtx"""
+	def __enter__(self):
+		global _db_ctx
+		self.should_cleanup=False
+		if not _db_ctx.is_init():
+			_db_ctx.init()
+			self.should_cleanup=True
+		return self
+
+	def __exit__(self,exctype,excvalue,traceback):
+		global _db_ctx
+		if self.should_cleanup:
+			_db_ctx.cleanup()
+
+def connection():
+	return _ConnectionCtx()
+
+def with_connection(func):
+	'''
+	Decorator for reuse connection
+	'''
+	@functools.wraps(func)
+	def _wrap(*args,**kw):
+		with _ConnectionCtx():
+			return func(*args,**kw)
+	return _wrap
+
+def _select(sql,first,*args):
+	global _db_ctx
+	cursor=None
+	sql = sql.replace('?','%s')
+	try:
+		cursor = _db_ctx.connection.cursor()
+		cursor.execute(sql,args)
+		if curor.description:
+			names = [x[0] for x in cursor.description]
+		if first:
+			values = cursor.fetchone()
+
+class _Transaction(object):
+    pass
+
+class _TransactionCtx(object):
+	"""docstring for _TransactionCtx"""
+	def __enter__(self):
+		global _db_ctx
+		self.should_close_conn=False
+		if not _db_ctx.is_init():
+			_db_ctx.init()
+			self.should_close_conn=True
+		_db_ctx.transaction=_db_ctx.transaction+1
+		return self
+
+	def __exit__(self,exctype,excvalue,traceback):
+		global _db_ctx
+		_db_ctx.transaction=_db_ctx.transaction-1
+		try:
+			if _db_ctx.transaction==0:
+				if exctype is None:
+					self.commit()
+				else:
+					self.rollback()
+		finally:
+			if self.should_close_conn:
+				_db_ctx.cleanup()
+
+	def commit(self):
+		global _db_ctx
+		try:
+			_db_ctx.connection.commit()
+		except:
+			raise
+
+	def rollback(self):
+		global _db_ctx
+		_db_ctx.connection.rollback()
+
+def transaction():
+	return _TransactionCtx()
+
+def with_transaction(func):
+	@functools.wraps(func)
+	def _wrapper(*args,**kw):
+		with _TransactionCtx():
+			return func(*args,**kw)
+	return _wrapper
+
+
+
+def _select(sql,first,*args):
+	global _db_ctx
+	cursor=None
+	sql=sql.replace('?','%s')
+	try:
+		cursor=_db_ctx.connection.cursor()
+		cursor.execute(sql,*args)
+		if cursor.description:
+			names=[x[0] for x in cursor.description]
+		if first:
+			values=cursor.fetchone()
+			if not values:
+				return None
+			return Dict(names,values)
+		return [Dict(names,x) for x in cursor.fetchall()]
+	finally:
+		if cursor:
+			cursor.close()
+
+@with_connection
+def select_one(sql,*args):
+	return _select(sql,True,*args)
+
+@with_connection
+def select_int(sql,*args):
+	d=_select(sql,True,*args)
+	if len(d) !=1:
+		raise MultiConlumnsError()
+	return d.values()[0]
+		raise MultiColumnsError()
+	return d.values()[0]
+
+@with_connection
+def select(sql,*args):
+	return _select(sql,False,*args)
+
+
+@with_connection
+def _update(sql,*args):
+	global _db_ctx
+	cursor=None
+	sql=sql.replace('?','%s')
+	try:
+		cursor=_db_ctx.connection.cursor()
+		cursor.excute(sql,*args)
+		r=cursor.rawcount
+		if _db_ctx.transaction==0:
+			_db_ctx.connection.commit()
+		return r
+	finally:
+		if cursor:
+			cursor.close()
+
+def insert(table,*kwargs):
+	cols,args = zip(*kwargs.iteritems())
+	sql = "insert into %s (%s) values(%s)"%(table,','.join(cols),','.join(['?' for i in range(len(cols))]))
+	return _update(sql,*args)
+
+def update(sql,*args):
+	return _update(sql,*args)
--- www/transwarp/orm.py
+++ www/transwarp/orm.py
@@ -2,9 +2,219 @@
 
 __author__ = 'hunter'
 
-import logging
+import logging,time
 from transwarp import db
 try:
 	from cStringIO import cStringIO
 except:
 	from StringIO import StringIO
+
+
+class Field(object):
+	"""docstring for Field"""
+	_count = 0
+	def __init__(self, **kw):
+		self.name= kw.get('name',None)
+		self._default=kw.get('default',None)
+		self.primary_key = kw.get('primary_key',False)
+		self.nullable = kw.get('nullable',False)
+		self.updateable=kw.get('updateable',True)
+		self.inertable=kw.get('insertable',True)
+		self.ddl=kw.get('ddl','')
+		self._order=Field._count
+		Field._count=Field._count+1
+
+	@property
+	def default(self):
+		d=self._default
+	    return d() if callable(d) else d
+
+	def __str__(self):
+		s=['<%s:%s,%s,default(%s),'%(self.__class__.__name__,self.name,self.ddl,self._default)]
+		self.nullable and s.append('N')
+		self.updateable and s.append('U')
+		self.inertable and s.append('I')
+		s.append('>')
+		return ''.join(s)
+
+
+
+class StringField(Field):
+	"""docstring for StringField"""
+	def __init__(self, **kw):
+		if not 'default' in kw:
+			kw['default']=''
+		if not 'ddl' in kw:
+			kw['ddl']='varchar(255)'
+		super(StringField, self).__init__(**kw)
+
+class IntegerField(Field):
+	"""docstring for IntegerField"""
+	def __init__(self, **kw):
+		if not 'default' in kw:
+			kw['default'] =0
+		if not 'ddl' in kw:
+			kw['ddl']='bigint'
+		super(IntegerField, self).__init__(**kw)
+
+class FloatField(Field):
+	"""docstring for FloatField"""
+	def __init__(self, **kw):
+		if not 'default' in kw:
+			kw['default'] =0.0
+		if not 'ddl' in kw:
+			kw['ddl']='real'
+		super(FloatField, self).__init__(**kw)
+
+class DateField(Field):
+	"""docstring for DateField"""
+	def __init__(self, **kw):
+		if not 'default' in kw:
+			kw['default'] =
+		if not 'ddl' in kw:
+			kw['ddl']=
+		super(DateField, self).__init__(**kw)
+class Boolean(Field):
+	"""docstring for Boolean"""
+	def __init__(self, **kw):
+		if not 'default' in kw:
+			kw['default'] =False
+		if not 'ddl' in kw:
+			kw['ddl']='bool'
+		super(Boolean, self).__init__(**kw)
+
+class BlobField(Field):
+	"""docstring for BlobField"""
+	def __init__(self, **kw):
+		if not 'default' in kw:
+			kw['default'] = ''
+		if not 'ddl' in kw:
+			kw['ddl'] = 'blob'
+		super(BlobField, self).__init__(**kw)
+		
+class VersionField(Field):
+	"""docstring for ClassName"""
+	def __init__(self, name=None):
+		super(VersionField, self).__init__(name=name,default=0,ddl='bigint')
+
+_triggers = frozenset(['pre_insert','pre_update','pre_delete'])
+
+def _gen_sql(table_name,mappings):
+	pk=None
+	sql=['--generating SQL for %s:' %table_name,'create table `%s`('%table_name]
+    for f in sorted(mappings.values(),lambda x,y:cmp(x._order,y._order)):
+        if not hasattr(f,'ddl'):
+            raise StandardError('no ddl in field:%s'%f)
+        ddl=f.ddl
+        nullable=f.nullable
+        if f.primary_key:
+            pk=f.primary_key
+        sql.append(nullable and '%s %s'%(f.name,ddl) or '%s %s not null'%(f.name,ddl))
+    sql.append(' primary key(%s)'%pk)
+    sql.append(');')
+    return '\n'.join(sql)
+
+
+#MetaClass starts here
+
+class ModelMetaClass(type):
+	"""docstring for MetaClass"""
+	def __new__(cls,name,bases,attrs):
+        if name=='Model':
+            return type.__new__(cls,name,bases,attrs)
+        if not hasattr(cls,'subclasses'):
+            cls.subclasses={}
+        if not name in subclasses:
+            cls.subclasses['name']=name
+
+        mappings=dict()
+        primary_key=None
+        #Scaning ORMappings...
+        for k,v in attrs.iteritems():
+            if isinstance(v,Field):
+                if not v.name:
+                    v.name=k
+                if v.primary_key:
+                    raise StandardError('Cant defined more than 1 primary key in class %s'%name)
+                if v.updateable:
+                    v.updateable=False
+                if v.inertable:
+                    v.inertable=False
+                primary_key=v
+            mappings[k]=v
+        if not primary_key:
+            raise StandardError('None primary_key defined in class %s'%name)
+        for k in mappings.keys():
+            attrs.pop(k)
+        if not '__table__' in attrs:
+            attrs['__table__'] = name.lower()
+        attrs['__mapping__'] = mappings
+        attrs['__primary_key__']=primary_key
+        attrs['__sql__']=lambda self:_gen_sql(attrs['__table__'],mappings)
+        for trigger in _triggers:
+            if not trigger in attrs:
+                attrs['trigger']=None
+        return type.__new__(cls,name,bases,attrs)
+
+
+
+class Model(dict):
+	__metaclass__ = ModelMetaClass
+	
+    def __init__(self,**kw):
+        super(Model,self).__init__(**kw)
+
+    def __getattr__(self,key):
+        try:
+            return self[key]
+        except:
+            raise AttributeError(r"Dict object has no attribute '%s'"%key)
+
+    def __setattr__(self,key,value):
+        self[key]=value
+
+#query data by primary key 
+    @classmethod
+    def get(cls,pk):
+        d=db.select_one('select * from %s where %s=?'%(cls.__table__,cls.__primary_key__.name),pk)
+        return cls(**d) if d else None
+
+    @classmethod
+    def find_first(cls,where,*args):
+        d=db.select_one('select * from %s %'%(cls.__table__,where),*args)
+        return cls(**d) if d else None
+
+    @classmethod
+    def find_by(cls,where,*args):
+        L=db.select('select * from %s %s'%(cls.__table__,where),*args)
+        return [cls(**d) for d in L]
+
+    @classmethod
+    def find_all(cls,*args):
+        l=db.select('select * from %s'%(cls.__table__))
+        return[cls(**d) for d in l]
+
+    @classmethod
+    def count_all(cls):
+        return db.select_int('select count(%s) from %s'%(cls.__primary_key__,cls.__table__))
+
+    @classmethod
+    def count_by(cls,where,*args):
+        return db.select_int('select count(%s) from %s %s'%(cls.__primary_key__,cls.__table__,where),*args)
+
+    def update(self):
+        pass
+
+    def insert(self):
+        self.pre_insert and self.pre_insert()
+        params={}
+        for k,v in self.__mapping__.iteritems():
+            if v.insertable:
+                if not hasattr(self,k):
+                    setattr(self,k,v.default)
+                params[v.name]=getattr(self,k)
+        db.insert('%s'%self.__table__,**params)
+        return self
+
+    def delete():
+        pass
